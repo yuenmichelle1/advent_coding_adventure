33873e30de35d2425f9a89ad77a760ecc78074ed
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-12-15 22:48:05 -0600

    day1

---
+++ 1/day1_part2.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+cal_tracker = [0, 0, 0]
+current_cals = 0
+File.read('./day1_inputs.txt').split("\n") do |line|
+  line = line.strip
+  if line.empty?
+    if current_cals > cal_tracker[2]
+      cal_tracker.pop
+      cal_tracker.append(current_cals)
+      cal_tracker = cal_tracker.sort.reverse
+    end
+    current_cals = 0
+  else
+    current_cals += line.to_i
+  end
+end
+
+puts cal_tracker
