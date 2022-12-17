afd783a88154506fc3945833f1555f55c236a4ec
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-12-17 00:00:12 -0600

    day3 finished part 2

--- 3/day3.rb
+++ 3/day3.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 ALPHABET = ('a'..'z').to_a
-score = 0
-
+# a - z score 1 - 26
+# A - Z score 27 - 52
 def calculate_priority(item)
   if ALPHABET.index(item).nil?
     ALPHABET.index(item.downcase) + 27
@@ -11,12 +11,28 @@ def calculate_priority(item)
   end
 end
 
-File.open('./input.txt').each_line do |line|
-  first_compartment = line[0...(line.length / 2)].split('')
-  second_compartment = line[(line.length / 2)...(line.length - 1)].split('')
-  compartment_intersection_arr = first_compartment & second_compartment
-  similar_item = compartment_intersection_arr[0]
-  score += calculate_priority(similar_item)
+def part_one
+  score = 0
+  File.open('./input.txt').each_line do |line|
+    first_compartment = line[0...(line.length / 2)].split('')
+    second_compartment = line[(line.length / 2)...(line.length - 1)].split('')
+    similar_item = (first_compartment & second_compartment)[0]
+    score += calculate_priority(similar_item)
+  end
+
+  puts score
 end
 
-puts score
+def part_two
+  score = 0
+  all_rucksacks = File.read('./input.txt').split("\n")
+  all_rucksacks = all_rucksacks.map { |rucksack| rucksack.split('') }
+  elves_bags_split_in_groups = all_rucksacks.each_slice(3).to_a
+  elves_bags_split_in_groups.each do |group|
+    badge = (group[0] & group[1] & group[2])[0]
+    score += calculate_priority(badge)
+  end
+  puts score
+end
+
+part_two
