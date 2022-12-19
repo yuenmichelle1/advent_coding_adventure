a876f462daba6824b187b9970017238309955e8f
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-12-18 21:44:25 -0600

    clean up day 4. can do without sets. using ranges and cover?

--- 4/day4.rb
+++ 4/day4.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
-
-require 'set'
-
 def pair_sections(pair)
   pair.split(',').map do |section|
-    section_arr = section.split('-').map(&:to_i)
-    Set.new(Range.new(section_arr[0], section_arr[1]))
+    start, finish = section.split('-').map(&:to_i)
+    Range.new(start, finish)
   end
 end
 
@@ -13,18 +10,18 @@ def part_one(all_pairs)
   fully_contained_set_count = 0
   all_pairs.each do |pair|
     elf_one_section, elf_two_section = pair_sections pair
-    fully_contained_set_count += 1 if (elf_one_section <= elf_two_section) || (elf_two_section <= elf_one_section)
+    fully_contained_set_count += 1 if elf_one_section.cover?(elf_two_section) || elf_two_section.cover?(elf_one_section)
   end
   fully_contained_set_count
 end
 
 def part_two(all_pairs)
-  overlap_set_count = 0
+  overlap_count = 0
   all_pairs.each do |pair|
     elf_one_section, elf_two_section = pair_sections pair
-    overlap_set_count += 1 if elf_one_section.intersect? elf_two_section
+    overlap_count += 1 if elf_one_section.cover?(elf_two_section.first) || elf_two_section.cover?(elf_one_section.first)
   end
-  overlap_set_count
+  overlap_count
 end
 
 puts part_one(File.read('./input.txt').split("\n"))
