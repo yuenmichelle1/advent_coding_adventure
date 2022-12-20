80d2204125a47e987454c406650d97e699516e58
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-12-20 17:48:13 -0600

    not pretty but day 5 solved

--- 5/day5.rb
+++ 5/day5.rb
@@ -23,12 +23,47 @@ def stack_hash_map(sketch)
   stacks_hash
 end
 
-def do_instructions(instructions, _stacks_hash_map)
-  instructions.split("\n").map { instruction.split(' ').filter }
+def top_of_stacks(stack_hash)
+  top = []
+  stack_hash.each_value { |stack| top << stack[-1] }
+  top.join
+end
+
+def parse_instructions(instructions)
+  instructions.split("\n").map do |instruction|
+    instruction.split(' ').filter do |letter|
+      numeric? letter
+    end.map(&:to_i)
+  end
+end
+
+def do_instructions(instructions, stacks_hash)
+  instructions_arr = parse_instructions instructions
+  instructions_arr.each do |instruction|
+    num_to_move, from_row, to_row = instruction
+    stacks_in_from_row = stacks_hash[from_row.to_s]
+    stacks_to_move = stacks_in_from_row.reverse.take(num_to_move)
+    stacks_hash[from_row.to_s] = stacks_in_from_row.take(stacks_in_from_row.length - num_to_move)
+    stacks_in_to_row = stacks_hash[to_row.to_s].concat(stacks_to_move)
+    stacks_hash[to_row.to_s] = stacks_in_to_row
+  end
+  top_of_stacks(stacks_hash)
+end
+
+def do_crane_two_instructions(instructions, stacks_hash)
+  instructions_arr = parse_instructions instructions
+  instructions_arr.each do |instruction|
+    num_to_move, from_row, to_row = instruction
+    stacks_in_from_row = stacks_hash[from_row.to_s]
+    stacks_to_move = stacks_in_from_row.reverse.take(num_to_move).reverse
+    stacks_hash[from_row.to_s] = stacks_in_from_row.take(stacks_in_from_row.length - num_to_move)
+    stacks_in_to_row = stacks_hash[to_row.to_s].concat(stacks_to_move)
+    stacks_hash[to_row.to_s] = stacks_in_to_row
+  end
+  top_of_stacks(stacks_hash)
 end
 
 sketch, instructions = File.read('./input.txt').split("\n\n")
 
-puts stack_hash_map sketch
-
-puts instructions.split("\n")[0]
+puts do_instructions(instructions, stack_hash_map(sketch))
+puts do_crane_two_instructions(instructions, stack_hash_map(sketch))
