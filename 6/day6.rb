4a8e1d4a82c8fa034aafb336672fb96d813500d2
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-12-21 01:42:15 -0600

    nicer way to do day 6 with each_cons

--- 6/day6.rb
+++ 6/day6.rb
@@ -2,21 +2,19 @@
 
 input = File.read('./input.txt')
 
-def has_dups?(arr)
-  !arr.detect { |e| arr.count(e) > 1 }.nil?
-end
-
 def find_idx_where_uniq(input, chunk_size)
-  (chunk_size..input.length).each do |idx|
-    str = input.slice((idx - chunk_size)..(idx - 1))
-    next unless (has_dups?(str.chars)) == false
-
-    puts str
-    puts idx
-    break
+  input.chars.each_cons(chunk_size).with_index do |chars_arr, i|
+    return i + chunk_size if chars_arr.uniq.length == chunk_size
   end
 end
 
-find_idx_where_uniq(input, 4)
-find_idx_where_uniq(input, 14)
+def part_one(input)
+  find_idx_where_uniq(input, 4)
+end
 
+def part_two(input)
+  find_idx_where_uniq(input, 14)
+end
+
+puts part_one(input)
+puts part_two(input)
